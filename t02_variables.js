99f016ec20b89847b688df4d490e8af3da1d3e7b
24153ig <24153ig@hvhs.school.nz>
2026-04-29 00:01:01 +0000

    Apr 29, 2026, 12:01 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,7 +1,7 @@
 /* Paste your code from task 1 here */
 /****************************
 This is a block comment.
-Introduction to JavaScript
+Variables in JavaScript 
 ****************************/
 console.log("Running t02_variables.js")
 console.log("Hello my name is Isla")
