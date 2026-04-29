26d07b6ecc123829c5308558f24b5462a609ced0
24153ig <24153ig@hvhs.school.nz>
2026-04-29 00:30:19 +0000

    Apr 29, 2026, 12:30 AM

--- t03_maths.js
+++ t03_maths.js
@@ -11,13 +11,15 @@ let username = "Isla"
 let age = 15;
 let year = "2026"
 let pocketmoney = 20;
+let answer;
 /****************************
 Main code
 ****************************/
 console.log("Hi"+ username)
 console.log("As of"+ year + "you are"+ age + "years old")
 console.log("You have" + pocketmoney + "dollars")
-console.log("In ten years you will be" + "years old")
+answer = year + 10
+console.log("In ten years you will be" + "add" +years old")
 /****************************
 Functions 
 ****************************/
