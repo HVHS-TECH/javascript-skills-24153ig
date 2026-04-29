77957de740a00888fe41eab2bced3e9713ae4351
24153ig <24153ig@hvhs.school.nz>
2026-04-29 00:26:57 +0000

    Apr 29, 2026, 12:26 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,28 @@
 /* Paste your code from task 2 here */
+/****************************
+This is a block comment.
+Maths in JavaScript 
+****************************/
+console.log("Running t03_maths.js")
+console.log("Hello my name is Isla")
+
+//variables
+let username = "Isla"
+let age = 15;
+let year = "2026"
+let pocketmoney = 20;
+let poctetmoney + 10;
+/****************************
+Main code
+****************************/
+console.log("Hi"+ username)
+console.log("As of"+ year + "you are"+ age + "years old")
+console.log("You have" + pocketmoney + "dollars")
+console.log("In ten years you will be" + poctetmoney + 10 + "years old")
+/****************************
+Functions 
+****************************/
+
+
+// This is a single line comment
+
