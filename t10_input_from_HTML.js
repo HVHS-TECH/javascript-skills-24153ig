a85eed043712b9a5a485d9e48bf0a12946b8d4b4
24153ig <24153ig@hvhs.school.nz>
2026-05-11 21:41:48 +0000

    May 11, 2026, 9:41 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -28,7 +28,10 @@ function welcome(){
 welcome()
 
 function getFormInput(){
-  
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+  OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+
 }
 
 /****************************
