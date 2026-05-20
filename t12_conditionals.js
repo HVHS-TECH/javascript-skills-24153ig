5bbef3a7a426d5fa149c6080ef55155608c3ee3f
24153ig <24153ig@hvhs.school.nz>
2026-05-20 05:55:37 +0000

    May 20, 2026, 5:55 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,7 +1,7 @@
 /****************************
 This is a block comment.
 ****************************/
-console.log("Running t11_data_types.js")
+console.log("Running t12_conditionals.js")
 console.log("Hello my name is Isla")
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
@@ -41,23 +41,28 @@ Main code
 ****************************/
 OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
 OUTPUT.innerHTML += "<p>As of " + year + " you are "+ age + " years old</p>";
-
 console.log("Hi "+ userName)
 console.log("As of "+ year + " you are "+ age + " years old")
+
 Answer = year - age
 console.log("You were born in " + Answer)
 OUTPUT.innerHTML += "<p>You were born in " + Answer +"</p>";
+
 Answer = Num1 + age
 console.log("In ten years you will be " + Answer + " years old")
 OUTPUT.innerHTML += "<p>In ten years you will be " + Answer +" years old</p>";
 console.log("You have " + pocketMoney + " dollars")
 OUTPUT.innerHTML += "<p>You have " + pocketMoney +" dollars</p>";
+
 Answer = pocketMoney / num2
 console.log("You spend half of your money, now you have " + Answer + " dollars")
 OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + Answer +" dollars</p>";
+
 Answer = pocketMoney / num2 + num3
 console.log("Then you get $3, now you have " + Answer + " dollars" )
 OUTPUT.innerHTML += "<p> Then you get $3, now you have " + Answer +" dollars</p>";
+
+console.log (typeof(pocketMoney));
 if (pocketMoney < 4) {
 console.log("A chocolate bar costs $4 Sorry you CAN'T afford a chocolate bar");
 OUTPUT.innerHTML += "<p>A chocolate bar costs $4 Sorry you CAN'T afford a chocolate bar</p>";
