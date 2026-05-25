28ba59be57c2afc0ac7139203126e7fce01b3d2e
24153ig <24153ig@hvhs.school.nz>
2026-05-25 21:35:04 +0000

    May 25, 2026, 9:35 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -10,12 +10,14 @@ console.log("Starting Loop")
 /****************************
 Main code
 ****************************/
+function spaceForJavaScriptOutput(){
 for (let i = 100; i>=0; i--){
 console.log(i + "bottles of milk on the wall, ")
 OUTPUT.innerHTML = "<p>"+i+" bottles of milk on the wall</p>";
 console.log(i + "bottles of milk;")
 console.log("If one of those bottles should happen to fall.." )
 console.log(i - 1 + "bottles of milk on the wall!")
+}
 }
 
 
