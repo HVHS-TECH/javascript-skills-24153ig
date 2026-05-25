1c9dd9623c755d31d4c760bda844dd4f2ddc3c61
24153ig <24153ig@hvhs.school.nz>
2026-05-25 21:57:14 +0000

    May 25, 2026, 9:57 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -18,9 +18,9 @@ console.log(i + " bottles of milk on the wall, ")
 console.log(i + " bottles of milk;")
 console.log("If one of those bottles should happen to fall.." )
 console.log(i - 1 + " bottles of milk on the wall!")
-OUTPUT.innerHTML = "<p>"+i+" bottles of milk on the wall</p>";
-OUTPUT.innerHTML = "<p>"+i+" bottles of milk</p>";
-OUTPUT.innerHTML = "<p> If one of those bottles should happen to fall..</p>";
+OUTPUT.innerHTML += "<p>"+i+" bottles of milk on the wall</p>";
+OUTPUT.innerHTML += "<p>"+i+" bottles of milk</p>";
+OUTPUT.innerHTML += "<p> If one of those bottles should happen to fall..</p>";
 }
 }
 song()
